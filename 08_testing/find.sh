7c112f43151e6a3d70dc085abd4239aad7f068f7
NOK <92591333+potatochick2020@users.noreply.github.com>
2022-09-16 12:49:33 +0100

    update

--- 08_testing/find.sh
+++ 08_testing/find.sh
@@ -1,1 +1,8 @@
-exec isPrime-broken1 1 > output.txt
+#!/bin/bash
+rm -rf ans.txt c.txt d.txt tempc.txt tempd.txt;
+for number in `seq 1 99999`; do
+    ./isPrime-broken3 $number >> c.txt ; 
+    ./isPrime-broken4 $number >> d.txt ; 
+done
+diff ans.txt c.txt > tempc.txt;
+diff ans.txt d.txt > tempd.txt;
